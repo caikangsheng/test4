7bd03825ae25109c7b99cef7f38759f35c82e381
caikangsheng <caikangsheng@middol.com>
2019-08-20 17:42:15 +0800

    新增

---
+++ src/main/java/com/middol/activiti_demo/activiti/Ac.java
@@ -0,0 +1,12 @@
+package com.middol.activiti_demo.activiti;
+
+/**
+ * @author caikangsheng
+ * @date 2019/8/20 17:41
+ */
+public class Ac {
+    public static void main(String[] args) {
+
+        System.out.println("1234543");
+    }
+}
